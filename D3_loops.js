4a61f27a939825aff4d6105609bd086f3a6ce7b1
Spencer Rauch <rauchs@gmail.com>
2023-11-29 13:42:00 -0800

    Jeremiah's loop demo

--- D3_loops.js
+++ D3_loops.js
@@ -42,3 +42,15 @@ while (whileNum > 0){ // it is important in a while loop to be working towards b
     //
     whileNum--
 }
+
+
+// Jeremiah Fernandez provided this to demo nested loops
+for (i=1; i<=3; i++) {
+    console.log("OUTER. This is the first and therefore outer loop. It counts everytime it runs.", i)
+    for (x=1; x<=3; x++) {
+        console.log("MIDDLE. This is the second and therefore middle tier loop. Look at it count inside.", x)
+        for (y=1; y<=3; y++) {
+            console.log ("INNER. This is the last function, and most inner part of the loop. Watch it count inside.", y)
+        }
+    }
+}
